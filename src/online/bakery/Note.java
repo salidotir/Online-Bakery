183f5ef916d6f9926464c56b5aa4438825d940f9
Sara Limooee <35997721+salidotir@users.noreply.github.com>
2020-12-17 00:36:58 +0330

    add Note version 1

---
+++ src/online/bakery/Note.java
@@ -0,0 +1,128 @@
+/*
+ * Notes to be added to an order by:
+ * 1. seller
+ * 2. customer
+ * 3. delivery employee
+ */
+package online.bakery;
+
+/**
+ *
+ * @author salidotir
+ */
+public class Note {
+    private final String id;
+    
+    private final String orderId;
+    private final String noteSellerId;
+    private final String noteCustomerId;
+    private String noteEmployeeId;          // maybe no need to employee for delivering the order
+    
+    private String noteSellerText;          // any note seller wants to add
+    private String noteCustomerText;        // any note customer wants to add
+    private String noteEmployeeText;        // any note delivery employee wants to add
+    
+    /**
+     * 
+     * @param id
+     * @param orderId 
+     * set these 2 parameters in the constructor, if customer or employee added a note, set their id fields.
+     */
+    public Note(String id, String orderId, String sellerId, String customerId) {
+        this.id = id;
+        
+        this.orderId = orderId;
+        this.noteSellerId = sellerId;
+        this.noteCustomerId = customerId;
+        this.noteEmployeeId = null;
+        
+        this.noteSellerText = "";
+        this.noteCustomerText = "";
+        this.noteEmployeeText = "";
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @return the orderId
+     */
+    public String getOrderId() {
+        return orderId;
+    }
+    
+    /**
+     * @return the noteSellerId
+     */
+    public String getNoteSellerId() {
+        return noteSellerId;
+    }
+
+    /**
+     * @return the noteCustomerId
+     */
+    public String getNoteCustomerId() {
+        return noteCustomerId;
+    }
+
+    /**
+     * @return the noteEmployeeId
+     */
+    public String getNoteEmployeeId() {
+        return noteEmployeeId;
+    }
+
+    /**
+     * @param noteEmployeeId the noteEmployeeId to set
+     */
+    public void setNoteEmployeeId(String noteEmployeeId) {
+        this.noteEmployeeId = noteEmployeeId;
+    }
+    
+    
+    /**
+     * @return the noteSellerText
+     */
+    public String getNoteSellerText() {
+        return noteSellerText;
+    }
+
+    /**
+     * @param noteSellerText the noteSellerText to set
+     */
+    public void setNoteSellerText(String noteSellerText) {
+        this.noteSellerText = noteSellerText;
+    }
+
+    /**
+     * @return the noteCustomerText
+     */
+    public String getNoteCustomerText() {
+        return noteCustomerText;
+    }
+
+    /**
+     * @param noteCustomerText the noteCustomerText to set
+     */
+    public void setNoteCustomerText(String noteCustomerText) {
+        this.noteCustomerText = noteCustomerText;
+    }
+
+    /**
+     * @return the noteEmployeeText
+     */
+    public String getNoteEmployeeText() {
+        return noteEmployeeText;
+    }
+
+    /**
+     * @param noteEmployeeText the noteEmployeeText to set
+     */
+    public void setNoteEmployeeText(String noteEmployeeText) {
+        this.noteEmployeeText = noteEmployeeText;
+    }
+}
