63636a2ba54fb813468a4da3271d871155703a26
NeginKhalifat <negin99kh@gmail.com>
2020-12-19 12:59:35 +0330

    add CakeBuilder

--- src/online/bakery/Cake.java
+++ src/online/bakery/Cake.java
@@ -3,11 +3,27 @@ package online.bakery;
 import java.math.BigDecimal;
 
 public class Cake extends Sweets {
-    public Cake(){
-        this.description="Cake";
+    public Cake(CakeBuilder builder) {
+        this.description = "Cake: ";
     }
+
     @Override
     public BigDecimal cost() {
         return new BigDecimal(0);
+    }
+
+    public static class CakeBuilder {
+
+        public CakeBuilder() {
+            Sweets sweets =new SugarCondimentDecorator.SugarCondimentDecoratorBuilder(new FlourCondimentDecorator.FlourCondimentDecoratorBuilder(new BakingPowderCondimentDecorator.BakingPowderCondimentDecoratorBuilder(new Cake(this), new BigDecimal(10)).build(),new BigDecimal(100)).build(),new BigDecimal(20)).build();
+            System.out.println(sweets.getDescription() + "\ntotal cost:  $"+ sweets.getTOTAL_COST()+ " total grams: "+sweets.getTOTAL_Grams());
+
+
+        }
+        public Cake build(){
+            return new Cake(this);
+        }
+
+
     }
 }
