6faab4b9f3fa42d04ffe3aca4ddf322eda8b45f8
Melika Z <32384849+m98z@users.noreply.github.com>
2020-12-21 02:11:35 +0330

    Complete PaymentStatus (V.1)

--- src/online/bakery/PaymentStatus.java
+++ src/online/bakery/PaymentStatus.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
  * @author melika
  */
-public class PaymentStatus {
-    
+public enum PaymentStatus {
+    IN_PROGRESS,        // it is geting to bank
+    SUCCESSFUL,        // cash recieved
+    UNSUCCESSFUL       // something went wrong with bank 
 }
