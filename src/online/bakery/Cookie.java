2cd8a6c7c709e61eabe0313509c17df653f668f7
NeginKhalifat <negin99kh@gmail.com>
2020-12-28 21:16:46 +0330

    add tart & cookie

---
+++ src/online/bakery/Cookie.java
@@ -0,0 +1,82 @@
+
+package online.bakery;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class Cookie extends Sweets {
+
+    public Cookie(CookieBuilder builder) {
+
+        this.description = "Cookie: ";
+        atomicInteger.incrementAndGet();
+        this.SweetId = atomicInteger.get();
+        this.TOTAL_COST = builder.TOTAL_COST;
+
+    }
+
+    @Override
+    public BigDecimal cost() {
+        return new BigDecimal(0);
+    }
+
+    public static class CookieBuilder {
+        protected String description;
+        protected BigDecimal TOTAL_Grams = new BigDecimal(0);
+        protected BigDecimal TOTAL_COST = new BigDecimal(0);
+
+
+        public CookieBuilder(ArrayList<DecoratorToBuild> decorators) {
+            Sweets cookie = new Cookie(this);
+
+            for (DecoratorToBuild decorator : decorators) {
+
+                switch (decorator.decorator_name) {
+                    case CHOCOLATE:
+                        break;
+                    case BACKGROUNDER:
+                        cookie = new BackingPowderCondimentDecorator(cookie, decorator.germs, decorator.cost);
+                        break;
+                    case SUGAR:
+                        cookie = new SugarCondimentDecorator(cookie, decorator.germs, decorator.cost);
+                        break;
+
+                    case FLOUR:
+
+                        cookie = new FlourCondimentDecorator(cookie, decorator.germs, decorator.cost);
+                        break;
+                    case VANILLA:
+                        cookie = new VanillaCondimentDecorator(cookie, decorator.germs, decorator.cost);
+                        break;
+                    case WALNUT:
+                        cookie = new WalnutCondimentDecorator(cookie, decorator.germs, decorator.cost);
+
+                        break;
+                    case STRAWBERRY:
+                        cookie = new StrawberryCondimentDecorator(cookie, decorator.germs, decorator.cost);
+
+                        break;
+                    case MILK:
+                        cookie = new MilkCondimentDecorator(cookie, decorator.germs, decorator.cost);
+
+                        break;
+                }
+            }
+
+            //Sweets sweets = new SugarCondimentDecorator.SugarCondimentDecoratorBuilder(new FlourCondimentDecorator.FlourCondimentDecoratorBuilder(new BackingPowderCondimentDecorator.BakingPowderCondimentDecoratorBuilder(, new BigDecimal(10)).build(), new BigDecimal(100)).build(), new BigDecimal(20)).build();
+            System.out.println("ID: " + cookie.SweetId + "\n" + cookie.getDescription() + "\ntotal cost:  $" + cookie.getTOTAL_COST() + " total grams: " + cookie.getTOTAL_Grams());
+            TOTAL_Grams = cookie.getTOTAL_Grams();
+            TOTAL_COST = cookie.getTOTAL_COST();
+            description = cookie.getDescription();
+        }
+
+        public Cookie build() {
+            atomicInteger.decrementAndGet();
+
+            return new Cookie(this);
+
+        }
+
+
+    }
+}
