df14a44ecaf62a041be1e465a4d077e3d8464e09
Sara Limooee <35997721+salidotir@users.noreply.github.com>
2020-12-28 20:47:24 +0330

    add new status for order-status

--- src/online/bakery/OrderStatus.java
+++ src/online/bakery/OrderStatus.java
@@ -8,9 +8,10 @@ package online.bakery;
  * @author salidotir
  */
 public enum OrderStatus {
-    PENDING,            // can be canceled
-    IN_PROGRESS,        // it is becoming ready
-    DONE,               // it is ready but not delivered
-    DELIVERED,          // it is delivered to customer
-    CANCELED            // customer can cancel the order in PENDING mode
+    PENDING,                // can be canceled
+    IN_PROGRESS,            // it is becoming ready
+    DONE,                   // it is ready but not delivered
+    DELIVERED,              // it is delivered to customer
+    CANCELED_BY_CUSTOMER,   // customer can cancel the order in PENDING mode
+    CANCELED_BY_BAKER       // baker can cancel an order and does not accept it
 }
