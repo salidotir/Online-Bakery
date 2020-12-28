5bc40a348324bcb00d4e193ba9d63cec52698e1a
NeginKhalifat <negin99kh@gmail.com>
2020-12-28 20:02:14 +0330

    add main

---
+++ src/online/bakery/main.java
@@ -0,0 +1,20 @@
+package online.bakery;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class main {
+    public static void main(String[] args){
+        ArrayList ss=new ArrayList<Sweets>();
+        ArrayList<DecoratorToBuild>decorators=new ArrayList<DecoratorToBuild>();
+        decorators.add(new DecoratorToBuild(Decorator.FLOUR,new BigDecimal(100),new BigDecimal(400)));
+
+        decorators.add(new DecoratorToBuild(Decorator.BACKGROUNDER,new BigDecimal(200),new BigDecimal(500)));
+        Sweets s1=new Cake.CakeBuilder(decorators).build();
+        System.out.println(s1.getTOTAL_COST());
+        ss.add(s1);
+        //System.out.println(ss);
+
+
+    }
+}
