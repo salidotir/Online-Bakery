0ba871dba33640af8842202f0489cc7bdc81ab74
Sara Limooee <35997721+salidotir@users.noreply.github.com>
2020-12-28 21:00:18 +0330

    fix payment-id field

--- src/online/bakery/Payment.java
+++ src/online/bakery/Payment.java
@@ -4,31 +4,33 @@
 package online.bakery;
 
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
  * @author salidotir
  */
 public class Payment {
-    private final String paymentId;
+    int paymentId ;
+    static AtomicInteger atomicInteger = new AtomicInteger(1);
     private final String paymentCustomerId;
     private final Date paymentDate;
     private final int paymentAmount;          // payment amount is calculated in 'Rials' -> int
     private String paymentDiscreption;  // any discreption for payment
     
     // constructor for creating payment object the first time for ordeing
-    public Payment(String paymentId, String paymentCustomerId, Date paymentDate, int paymentAmount) {
-        this.paymentId = paymentId;
+    public Payment(String paymentCustomerId, Date paymentDate, int paymentAmount) {
         this.paymentCustomerId = paymentCustomerId;
         this.paymentDate = paymentDate;
         this.paymentAmount = paymentAmount;
         this.paymentDiscreption = "";
+        this.paymentId = atomicInteger.incrementAndGet();
     }
 
     /**
      * @return the paymentId
      */
-    public String getPaymentId() {
+    public int getPaymentId() {
         return paymentId;
     }
 
