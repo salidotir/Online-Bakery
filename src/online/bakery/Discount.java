7557cd65fb87d687543c1e4cda180824caf0a67a
fatima-msd <fatema.m443@gmail.com>
2020-12-28 18:30:25 +0330

    Add Discount class V.1

---
+++ src/online/bakery/Discount.java
@@ -0,0 +1,43 @@
+package online.bakery;
+
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Discount {
+    private int id ;
+    static AtomicInteger atomicInteger = new AtomicInteger(2);
+    private String name;
+    private int percent;
+    private Date start;
+    private Date end;
+    private int confectionerId;
+
+    public Discount(String name , int percent,Date start , Date end,int confectionerId) {
+        atomicInteger.incrementAndGet();
+        this.id=atomicInteger.incrementAndGet();
+        this.name = name;
+        this.percent = percent;
+        this.start = start;
+        this.end = end;
+        this.confectionerId = confectionerId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPercent(int percent) {
+        this.percent = percent;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+
+}
