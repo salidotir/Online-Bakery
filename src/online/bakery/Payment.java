f38baf2940b153b9856d31bd2efc99c3439cef80
Sara Limooee <35997721+salidotir@users.noreply.github.com>
2020-12-29 15:41:57 +0330

    add pay-from-wallet & pay-online functions

--- src/online/bakery/Payment.java
+++ src/online/bakery/Payment.java
@@ -5,6 +5,7 @@ package online.bakery;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -13,7 +14,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class Payment {
     int paymentId ;
-    static AtomicInteger atomicInteger = new AtomicInteger(1);
+    static AtomicInteger atomicInteger = new AtomicInteger(0);
     private final int customerId;
     private final int orderId;
     private final Date date;
@@ -23,7 +24,7 @@ public class Payment {
     private PaymentType paymentType;
     
     // constructor for creating payment object the first time for ordeing
-    public Payment(int paymentOrderId, int paymentCustomerId, Date paymentDate, BigDecimal paymentAmount, String paymentDesciption, PaymentStatus paymentStatus, PaymentType paymentType) {
+    public Payment(int paymentOrderId, int paymentCustomerId, Date paymentDate, BigDecimal paymentAmount, String paymentDesciption, PaymentType paymentType) {
         this.orderId = paymentOrderId;
         this.customerId = paymentCustomerId;
         this.date = paymentDate;
@@ -31,8 +32,8 @@ public class Payment {
         this.discreption = paymentDesciption;
         
         this.paymentId = atomicInteger.incrementAndGet();
-        this.paymentStatus = paymentStatus;
         
+        this.paymentStatus = PaymentStatus.IN_PROGRESS;
         this.paymentType = paymentType;
     }
 
@@ -112,5 +113,53 @@ public class Payment {
     public void setPaymentType(PaymentType paymentType) {
         this.paymentType = paymentType;
     }
-
+    
+    public static PaymentType howToPay() {
+        int index = 1;
+        for (PaymentType PT: PaymentType.values())
+        {
+            System.out.println(index + " : "+ PT);
+            index += 1;
+        }
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("Please Enter how you want to pay for the order: ");
+        int num = scan.nextInt();
+        //scan.close();
+        return PaymentType.values()[num-1];
+    }
+    
+    public boolean pay(Payment pay, Customer customer) {
+        if(pay.getPaymentType() == PaymentType.FROM_WALLET) {
+            return this.payFromWallet(pay, customer);
+        }
+        else if(pay.getPaymentType() == PaymentType.PAY_ONLINE) {
+            return this.payOnline(pay);
+        }
+        pay.setPaymentStatus(PaymentStatus.UNSUCCESSFUL);
+        return false;
+    }
+    
+    private boolean payFromWallet(Payment pay, Customer customer) {
+        int res = customer.getWallet().getAmount().compareTo(pay.paymentAmount);
+        // res == 0 -> both are equal
+        // res == 1 -> wallet > pay amount
+        // res == -1 -> pay amount > wallet
+        if(res == 0 || res == 1) {
+            customer.getWallet().subtractAmount(pay.paymentAmount);
+            pay.setPaymentStatus(PaymentStatus.SUCCESSFUL);
+            return true;
+        }
+        else {
+            pay.setPaymentStatus(PaymentStatus.UNSUCCESSFUL);
+            System.out.println("There is not enough money in the wallet.\n");
+            return false;
+        }
+    }
+    
+    private boolean payOnline(Payment pay) {
+        pay.setPaymentStatus(PaymentStatus.SUCCESSFUL);
+        System.out.println("Paid for the order online.\n");
+        return true;
+    }
+    
 }
