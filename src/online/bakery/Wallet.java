53885cf4bfcc15056984937a73c1e93a6ec9e3fc
Sara Limooee <35997721+salidotir@users.noreply.github.com>
2020-12-29 14:49:45 +0330

    add get-wallet-information() function

--- src/online/bakery/Wallet.java
+++ src/online/bakery/Wallet.java
@@ -4,18 +4,27 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
- * @author melika
+ * @author salidotir
  */
 public class Wallet {
     private int Amount;
     private final int walletId ;
-    static AtomicInteger atomicInteger = new AtomicInteger(2);
+    static AtomicInteger atomicInteger = new AtomicInteger(0);
     
     public Wallet() {
         this.Amount = 0;
         this.walletId=atomicInteger.incrementAndGet();
     }
         
+    public String getWalletInformation() {
+        String s;
+        s = "**Wallet infrmation**\n" + 
+                "wallet id: " + walletId + "\n" +
+                "amount: " + Amount + "\n" + 
+                "____________________________\n";
+        return s;
+    }
+    
     public int getWalletId() {
         return this.walletId;
     }
