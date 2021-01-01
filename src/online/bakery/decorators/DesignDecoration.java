d5ac10fd17ae2a22959030a25f002a9fedec9e6f
NeginKhalifat <negin99kh@gmail.com>
2021-01-01 23:19:03 +0330

    add DesignDecoration for decorating cake

---
+++ src/online/bakery/decorators/DesignDecoration.java
@@ -0,0 +1,11 @@
+package online.bakery.decorators;
+
+public enum DesignDecoration {
+    CHOCOLATE,
+    BLUEBERRY,
+    STRAWBERRY,
+    SIMPLE_FONDANT,
+    COCONUT_POWDER,
+    CREAM,
+    SMARTIES,
+}
