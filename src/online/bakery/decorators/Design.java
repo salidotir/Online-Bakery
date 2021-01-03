fc3c2fdea3af11b195081f20b0f81a3a5baa5316
NeginKhalifat <negin99kh@gmail.com>
2021-01-03 22:21:00 +0330

    fix design (add cake)

--- src/online/bakery/decorators/Design.java
+++ src/online/bakery/decorators/Design.java
@@ -1,15 +1,23 @@
 package online.bakery.decorators;
 
+import online.bakery.sweets.Cake;
 import online.bakery.sweets.Color;
+import online.bakery.sweets.Sweets;
 
 import java.util.ArrayList;
 
 public class Design {
     protected ArrayList<Color>colors;
     protected ArrayList<DesignDecoration>DesignDecorations;
-    public Design(ArrayList<Color>colors,ArrayList<DesignDecoration>designDecorations){
+    protected Sweets cake;
+    public Design(ArrayList<Color>colors,ArrayList<DesignDecoration>designDecorations,Sweets cake){
         this.colors=colors;
         this.DesignDecorations=designDecorations;
+        this.cake=cake;
+    }
+
+    public Sweets getCake() {
+        return cake;
     }
 
     public ArrayList<Color> getColors() {
